6f02abe26acc3658c11643613bd01780201118c4
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-03-17 09:57:13 +0100

    TG.StockGen.TechHediff

---
+++ Source/StockGen/TechHediff.cs
@@ -0,0 +1,27 @@
+using RimWorld;
+using Verse;
+
+namespace TG.StockGen
+{
+	public class TechHediff : ConditionMatcher
+	{
+		protected override bool CanBuy(in ThingDef def)
+		{
+			return def.tradeTags != null && def.tradeTags.Contains("TechHediff");
+		}
+
+		private static readonly SimpleCurve SelectionWeight = new SimpleCurve
+		{
+			new CurvePoint(50.0f, 0.3f),
+			new CurvePoint(400.0f, 0.6f),
+			new CurvePoint(800.0f, 1.0f),
+			new CurvePoint(1000.0f, 0.8f),
+			new CurvePoint(1300f, 0.6f),
+			new CurvePoint(1600f, 0.3f),
+			new CurvePoint(2000f, 0.1f)
+		};
+
+		protected override float Weight(in ThingDef def, in int forTile, in Faction faction) =>
+			SelectionWeight.Evaluate(def.BaseMarketValue);
+	}
+}
