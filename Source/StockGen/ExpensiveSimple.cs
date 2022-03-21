88ade78614321a7641310f76c1064c5ced4d2201
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-03-21 17:36:38 +0100

    Improve comments

--- Source/StockGen/ExpensiveSimple.cs
+++ Source/StockGen/ExpensiveSimple.cs
@@ -5,9 +5,9 @@ using Verse;
 namespace TG.StockGen
 {
 	/// <summary>
-	/// Purchase anything with a high enough market value per unit of volume.
-	/// Fulfills a similar role than StockGenerator_BuyExpensiveSimple, but it ignores additional objects such as
-	/// edible items or relics
+	/// Handles anything with a high enough market value per unit of volume.
+	/// Intended to replace StockGenerator_BuyExpensiveSimple. It ignores additional objects such as organs, eggs or
+	/// relics. If needed it can also be used to generate random, high-value stock.
 	/// </summary>
 	public class ExpensiveSimple : ConditionMatcher
 	{
@@ -16,6 +16,9 @@ namespace TG.StockGen
 		/// </summary>
 		public float minValuePerUnit = 10f;
 
+		/// <summary>
+		/// Lazily initialized cache of disallowed things. They belong to one of the categories in CanBuy.
+		/// </summary>
 		private static HashSet<ThingDef> _disallowedThings;
 
 		protected override bool CanBuy(in ThingDef def)
