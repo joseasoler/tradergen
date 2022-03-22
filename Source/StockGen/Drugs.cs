3c5bb218184dc01e72d45d3a11c6d1a2b1acf926
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-03-22 16:45:16 +0100

    TG.StockGen.Drugs now has a drugCategory support.

--- Source/StockGen/Drugs.cs
+++ Source/StockGen/Drugs.cs
@@ -8,10 +8,17 @@ namespace TG.StockGen
 	/// </summary>
 	public class Drugs : ConditionMatcher
 	{
+		/// <summary>
+		/// If set to an specific value, it will only generate drugs from the chosen category.
+		/// </summary>
+		public DrugCategory drugCategory = DrugCategory.Any;
+
 		protected override bool CanBuy(in ThingDef def)
 		{
-			return def.IsWithinCategory(ThingCategoryDefOf.Drugs) && !Util.IsAlcohol(def) && (def.thingCategories == null ||
-				!def.thingCategories.Any(cat => cat.defName == "VBE_DrinksNonAlcoholic"));
+			return def.IsWithinCategory(ThingCategoryDefOf.Drugs) && !Util.IsAlcohol(def) &&
+			       (def.thingCategories == null ||
+			        !def.thingCategories.Any(cat => cat.defName == "VBE_DrinksNonAlcoholic")) &&
+			       def.IsIngestible && (drugCategory == DrugCategory.Any || def.ingestible.drugCategory == drugCategory);
 		}
 	}
 }
