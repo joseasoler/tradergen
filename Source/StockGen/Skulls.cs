a80aa9ee8e34d8ae0566a5847358567809b467b0
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-15 10:32:21 +0200

    TG_Skullspike_Desired

---
+++ Source/StockGen/Skulls.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace TG.StockGen
+{
+	/// <summary>
+	/// Purchases and sells skulls. Each skull gets a person name from a random culture and gender. Requires Ideology.
+	/// </summary>
+	public class Skulls : ConditionMatcher
+	{
+		public Skulls()
+		{
+			thingDefCountRange = IntRange.one;
+		}
+
+		protected override IEnumerable<Thing> TryMakeForStock(ThingDef def, Faction faction)
+		{
+			if (!ModsConfig.IdeologyActive)
+			{
+				yield break;
+			}
+
+			for (var idx = 0; idx < RandomCountOf(ThingDefOf.Skull); ++idx)
+			{
+				var thing = ThingMaker.MakeThing(ThingDefOf.Skull);
+				thing.stackCount = 1;
+				var sourceComp = thing.TryGetComp<CompHasSources>();
+				var name = PawnBioAndNameGenerator.GenerateFullPawnName(ThingDefOf.Human,
+					primaryCulture: DefDatabase<CultureDef>.AllDefs.RandomElement(),
+					gender: Rand.Value < 0.5 ? Gender.Male : Gender.Female);
+				sourceComp.AddSource(name.ToStringShort);
+
+				yield return thing;
+			}
+		}
+
+		protected override bool CanBuy(in ThingDef def)
+		{
+			return ModsConfig.IdeologyActive && def == ThingDefOf.Skull;
+		}
+	}
+}
