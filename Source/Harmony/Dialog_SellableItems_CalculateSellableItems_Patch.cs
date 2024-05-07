61c3b2868623bb3e998c96a3a56434c841f1330d
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-05-07 18:19:40 +0200

    Fix what will buy settlement button. Fixes #2.

--- Source/Harmony/Dialog_SellableItems_CalculateSellableItems_Patch.cs
+++ Source/Harmony/Dialog_SellableItems_CalculateSellableItems_Patch.cs
@@ -1,18 +1,48 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
 using RimWorld;
+using TraderGen.TraderKind;
+using Verse;
 
 namespace TraderGen.Harmony
 {
 	/// <summary>
-	/// Make TraderGen take over the calculation of sellable items.
-	/// See Dialog_SellableItems_Constructor_Patch for details.
+	/// Use the TraderGen cache to determine tradeability.
 	/// </summary>
 	[HarmonyPatch(typeof(Dialog_SellableItems), nameof(Dialog_SellableItems.CalculateSellableItems))]
 	internal static class Dialog_SellableItems_CalculateSellableItems_Patch
 	{
-		private static bool Prefix()
+		public static bool WillTrade(TraderKindDef traderKindDef, ThingDef thingDef, ITrader trader)
 		{
-			return false;
+			return Cache.WillTrade(trader, traderKindDef, thingDef);
+		}
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo vanillaWillTradeMethod =
+				AccessTools.Method(typeof(TraderKindDef),
+					nameof(TraderKindDef.WillTrade));
+
+			MethodInfo modifiedWillTradeMethod =
+				AccessTools.Method(typeof(Dialog_SellableItems_CalculateSellableItems_Patch), nameof(WillTrade));
+
+			FieldInfo traderField = AccessTools.Field(typeof(Dialog_SellableItems), nameof(Dialog_SellableItems.trader));
+
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if (instruction.Calls(vanillaWillTradeMethod))
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0); // this
+					yield return new CodeInstruction(OpCodes.Ldfld, traderField); // RimWorld.Dialog_SellableItems::trader
+					yield return new CodeInstruction(OpCodes.Call, modifiedWillTradeMethod);
+				}
+				else
+				{
+					yield return instruction;
+				}
+			}
 		}
 	}
 }
